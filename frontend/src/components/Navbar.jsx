cc2192e780d42ad6d75bd79d6fc9a5442dd8520b
Ksenia <73356609+Kse-Nia@users.noreply.github.com>
2023-04-24 16:26:08 +0200

    add navlinks

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import { Link } from "react-router-dom";
 import Logo from "../assets/LOGO.svg";
 
 const Navbar = () => {
@@ -9,12 +10,20 @@ const Navbar = () => {
         <div className="navbar_logo">
           <img src={Logo} className="navbar_logo-image" alt="logo" />
         </div>
-        {/*    <NavLink className="navbar-link" to="/">
-          Accueil
-        </NavLink>
-        <NavLink className="navbar-link" to="/about" end>
-          A Propos
-        </NavLink> */}
+        <div className="navbar_links">
+          <ul>
+            <li>
+              <Link className="navbar_links-link" to="/">
+                Accueil
+              </Link>
+            </li>
+            <li>
+              <Link className="navbar_links-link" to="/about">
+                A Propos
+              </Link>
+            </li>
+          </ul>
+        </div>
       </nav>
     </>
   );
