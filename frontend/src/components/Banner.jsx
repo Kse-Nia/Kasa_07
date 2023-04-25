24aa716a77e3c1b0bd0deec0cd25937743ae15a0
Ksenia <73356609+Kse-Nia@users.noreply.github.com>
2023-04-25 20:41:21 +0200

    add about banner and assets

--- frontend/src/components/Banner.jsx
+++ frontend/src/components/Banner.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 import BannerImage from "../assets/banner.png";
 
-const Banner = () => {
+const Banner = ({ picture, text }) => {
   return (
     <section className="banner_section">
-      <img className="banner_picture" src={BannerImage} alt="banner" />
-      <h1 className="banner_text">Chez vous, partout et ailleurs</h1>
+      <img
+        className="banner_picture"
+        /* src={BannerImage} */ src={picture}
+        alt="banner"
+      />
+      {/*  <h1 className="banner_text">Chez vous, partout et ailleurs</h1> */}
+      <h1 className="banner_text">{text}</h1>
     </section>
   );
 };
