c9fe6fb414795952d37c2cf12712c64b4c1a8312
Ksenia <73356609+Kse-Nia@users.noreply.github.com>
2023-04-25 21:50:52 +0200

    update show/close, add change icons

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -2,13 +2,14 @@ import React, { useState } from "react";
 import Navbar from "../components/Navbar";
 import Banner from "../components/Banner";
 import Picture from "../assets/banner2.png";
-import Icon from "../assets/row.svg";
+import IconOpen from "../assets/row.svg";
+import IconClose from "../assets/row2.svg";
 
 const About = () => {
-  const [openFiabilite, setOpenFiabilite] = useState(false);
+  const [openFiability, setOpenFiability] = useState(false);
   const [openRespect, setOpenRespect] = useState(false);
   const [openService, setOpenService] = useState(false);
-  const [openSecurite, setOpenSecurite] = useState(false);
+  const [openSecurity, setOpenSecurity] = useState(false);
 
   return (
     <>
@@ -19,13 +20,13 @@ const About = () => {
         <div className="modal_title">
           <h1>Fiabilité</h1>
           <img
-            src={Icon}
+            src={openFiability ? IconClose : IconOpen}
             alt="icon"
-            onClick={() => setOpenFiabilite((prev) => !prev)}
+            onClick={() => setOpenFiability((prev) => !prev)}
           />
         </div>
-        {openFiabilite && (
-          <p className="modal_text">
+        {openFiability && (
+          <p className="modal_text open">
             Les annonces postées sur Kasa garantissent une fiabilité totale. Les
             photos sont conformes aux logements, et toutes les informations sont
             régulièrement vérifiées par nos équipes.
@@ -36,13 +37,13 @@ const About = () => {
         <div className="modal_title">
           <h1>Respect</h1>
           <img
-            src={Icon}
+            src={openRespect ? IconClose : IconOpen}
             alt="icon"
             onClick={() => setOpenRespect((prev) => !prev)}
           />
         </div>
         {openRespect && (
-          <p className="modal_text">
+          <p className="modal_text open">
             La bienveillance fait partie des valeurs fondatrices de Kasa. Tout
             comportement discriminatoire ou de perturbation du voisinage
             entraînera une exclusion de notre plateforme.
@@ -53,13 +54,13 @@ const About = () => {
         <div className="modal_title">
           <h1>Service</h1>
           <img
-            src={Icon}
+            src={openService ? IconClose : IconOpen}
             alt="icon"
             onClick={() => setOpenService((prev) => !prev)}
           />
         </div>
         {openService && (
-          <p className="modal_text">
+          <p className="modal_text open">
             Nos équipes se tiennent à votre disposition pour vous fournir une
             expérience parfaite. N'hésitez pas à nous contacter si vous avez la
             moindre question.
@@ -70,13 +71,13 @@ const About = () => {
         <div className="modal_title">
           <h1>Sécurité</h1>
           <img
-            src={Icon}
+            src={openSecurity ? IconClose : IconOpen}
             alt="icon"
-            onClick={() => setOpenSecurite((prev) => !prev)}
+            onClick={() => setOpenSecurity((prev) => !prev)}
           />
         </div>
-        {openSecurite && (
-          <p className="modal_text">
+        {openSecurity && (
+          <p className="modal_text open">
             La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que
             pour les voyageurs, chaque logement correspond aux critères de
             sécurité établis par nos services. En laissant une note aussi bien à
